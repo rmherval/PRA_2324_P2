9a2d3fe9073b1fca09db40894373edff5d8441ea
RMHERVAL <rmherval@edu.upv.es>
2023-10-24 15:55:13 +0200

    Actualizados

--- DyV.h
+++ DyV.h
@@ -39,8 +39,60 @@ else if(v[medio]> x)
  else
    	return BusquedaBinaria_INV(x,v,ini, (medio-1));
 }
+  void swap(T* i, T* j){
+   T aux;
+   if((*i) > (*j)){
+       aux = (*i);
+       (*i) = (*j);
+       (*j) = aux;
+   }
+   }
+  /*Selecciono como pivote el último y voy comparando entre el primero y el pivote, si es más grande que el pivote no hago nada, aumento la j y comparo el siugiente y el pivote si es más pequeño entonces hago swap con el de la i y la j así el de la i siempre tendrá un valor mayor que el pivote y lo hiré subiendo hasta que acabe el for e intercambie el que es mayor con el pivote y deje el pivote en su sitio dejando los menosres a la izquierda y los mayores a la derecha*/
+  T Partition_f(std::vector<T>& v, int ini,int fin){//pivote final
+     T x = v[fin];//pivote
+    int i = ini;
+    for(int j=ini; j<fin; j++){
+      if(v[j] <=x){
+	swap(&v[i], &v[j]);
+	i++;
+      }
+    }
+    swap(&v[i], &v[fin]);
+    return i;
+  }
 
+  T Partition_p(std::vector<T>& v, int ini,int fin){//pivote primero
+     T x = v[ini];//pivote
+    int i = ini;
+    for(int j=ini+1; j<=fin; j++){
+      if(v[j] <=x){
+	swap(&v[i], &v[j]);
+	i++;
+      }
+    }
+    swap(&v[i], &v[fin]);
+    return i;
+  }
+    T Partition_m(std::vector<T>& v, int ini,int fin){//pivote medio
+      T x = v[(ini+fin)/2];//pivote
+    int i = ini;
+    for(int j=ini+1; j<=fin; j++){
+      if(v[j] <=x){
+	swap(&v[i], &v[j]);
+	i++;
+      }
+    }
+    swap(&v[i], &v[fin]);
+    return i;
+  }
 
+  void  QuickSort(std::vector<T>& v, int ini, int fin){
+    if(ini<fin){
+      int pivote = Partition(v, ini, fin);
+      QuickSort(v,ini,pivote-1);
+      QuickSort(v,pivote+1,fin);
+    }
+  }
 
 
 
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -9,19 +9,25 @@ int main(int argc, char** argv){
   std::vector<int>v{3,8,15,23,56,60,72,75,81,92};//ascendente
   std::vector<float>b{3.3,5.2,6.3,10.7,12.7};//ascendente
   std::vector<int>w{92,81,75,72,60,56,23,15,8,3};
+  std::vector<int>z{3,8,4,7,5,6,2};
 bbin<int> Objeto;
  bbin<float> Objeto2;
 
 auto start = std::chrono::system_clock::now();
 
-//Objeto.busqbinaria(atoi(argv[1]),v,0,9);//proceso a medir con enteros
-// Objeto2.busqbinaria(5.2,b,0,4);
- Objeto.BusquedaBinaria_INV(atoi(argv[1]),w,0,9);
+//Objeto.busqbinaria(atoi(argv[1]),v,0,v.size()-1);//proceso a medir con enteros
+// Objeto2.busqbinaria(5.2,b,0, b.size()-1);
+//Objeto.BusquedaBinaria_INV(atoi(argv[1]),w,0,w.size()-1);
+ Objeto.QuickSort(z,0,z.size()-1);
 
 auto end = std::chrono::system_clock::now(); //Guarda el instante
 std::chrono::duration<float, std::milli> duration = end - start; //Diferencia
 std::cout << "Time: " << duration.count() << "s" << std::endl; //Resultado
-	
+
+		  for(int i=0; i<z.size(); i++){
+		    std::cout<<z[i]<<",";
+		  }
+		  std::cout<<std::endl;
 return 0;
 
 }
